d223632bcb617e64d428ba6e64b5cae1c4955f60
patroN <khayrutdinovrizvan@gmail.com>
2024-12-21 13:39:03 +0300

    Final changes
    This reverts commit 66959f3d

--- src/main/java/ru/jrp/App.java
+++ src/main/java/ru/jrp/App.java
@@ -1,5 +1,6 @@
 package ru.jrp;
 
+import ru.jrp.entity.Doctor;
 import ru.jrp.repositories.DoctorRepository;
 import ru.jrp.repositories.MedicalCardRepository;
 import ru.jrp.repositories.PatientRepository;
@@ -17,30 +18,46 @@ public class App {
     private static final RecordRepository recordRepository = new RecordRepositoryImpl();
 
     public static void main(String[] args) {
+
         //SQL requests for table Doctors
         //int saveDoctor = doctorRepository.save(new Doctor(6L, "Ризван", "Стоматолог"));
         //System.out.println(saveDoctor);
+
         var getAllDoctors = doctorRepository.getAll();
         System.out.println(getAllDoctors);
+
         //int updateDoctor = doctorRepository.update(new Doctor(6L, "Ризван", "Хирург"));
         //System.out.println(updateDoctor);
+
         //int deleteDoctor = doctorRepository.delete(6L);
+
+
         //SQL requests for table Patients
         //int savePatient = patientRepository.save(new Patient(6L, "Ризван", "Хади Такташа, 121"));
         //System.out.println(savePatient);
+
         var getAllPatients = patientRepository.getAll();
         System.out.println(getAllPatients);
+
         //int updatePatient = patientRepository.update(new Patient(6L, "Ризван", "Меридианная, 1"));
         //System.out.println(updatePatient);
+
         //int deletePatient = patientRepository.delete(6L);
+
+
         //SQL requests for table Medical_cards
         //int saveMedCard = medicalCardRepository.save(new MedicalCard(6L, 1L, "Кариес"));
         //System.out.println(saveMedCard);
+
         var getAllMedCards = medicalCardRepository.getAll();
         System.out.println(getAllMedCards);
+
         //int updateMedCard = medicalCardRepository.update(new MedicalCard(6L, 1L, "Ушиб"));
         //System.out.println(updateMedCard);
+
         //int deleteMedCard = medicalCardRepository.delete(6L);
+
+
         //SQL requests for table Medical_cards
         //int saveRecord = recordRepository.save(new Record(12L, 1L, 5L,new Date(1723723920000L)));
         //System.out.println(saveRecord);
